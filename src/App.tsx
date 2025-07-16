f025d354ff136620d90c6d0d227af9871b32c297
sigma.lukman <auliya.lukman@sigma.co.id>
2025-07-16 11:15:30 +0700

    import important components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 
 import './App.css'
-import BaseLayout from './components/base_layout/baseLayout'
-import Login from './components/login/login'
+import BaseLayout from './components/base_layout/BaseLayout.tsx'
+import MealDataTable from './components/meal_datatable/MealDataTable'
+import data_table_meals from './data_sourcing_example/data_datatable'
+
+
 
 function App() {
 
 
   return (
     <>
-      
       <BaseLayout>
-        <Login arg="tesst" arg1={100} />
+        {/* <Login arg="tesst" arg1={100} /> */}
+        <MealDataTable data_source={data_table_meals}/>
+        
       </BaseLayout>
     </>
   )
