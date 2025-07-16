b1e2c34fa8d64949198801ff52f2d84b66ee41da
sigma.lukman <auliya.lukman@sigma.co.id>
2025-07-16 16:31:55 +0700

    add testing for MealDataTable component

---
+++ src/components/meal_datatable/MealDataTable.test.tsx
@@ -0,0 +1,16 @@
+
+import data_table_meals from "../../data_sourcing_example/data_datatable";
+import MealDataTable from "./MealDataTable";
+
+
+import { render, screen } from '@testing-library/react';
+import { test, expect } from 'vitest';
+
+
+test('renders MealDataTable component', () => {
+  render(<MealDataTable data_source={data_table_meals} />);
+  
+  expect(screen.getByText('Meal Name')).toBeInTheDocument()
+  expect(screen.getByText('Toast')).toBeInTheDocument()
+  expect(screen.getByText('Green Pea')).toBeInTheDocument()
+});
