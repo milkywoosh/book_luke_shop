1c46c84e23ebfe8584234cd8c3d8aff71247cfe5
sigma.lukman <auliya.lukman@sigma.co.id>
2025-07-16 19:51:46 +0700

    edit data source

--- src/data_sourcing_example/data_datatable.tsx
+++ src/data_sourcing_example/data_datatable.tsx
@@ -1,13 +1,16 @@
 const data_table_meals = [
     {
+        id: 1,
         meal_name: 'Toast',
         price: 100
     },
     {
+        id: 2,
         meal_name: 'Green Pea',
         price: 200
     },
     {
+        id: 3,
         meal_name: 'Oatmeal',
         price: 300
     }
---
+++ src/data_sourcing_example/data_meal_detail.tsx
@@ -0,0 +1,29 @@
+const data_meal_details: {
+    id: number;
+    meal_name: string;
+    intro: string,
+    nutrition_fact: string
+}[] = [
+    {
+        id: 1,
+        meal_name: 'Toast',
+        intro: `Don't forget to consume this in the morning, this meal can help you to fill your hungry, but with no worry.`,
+        nutrition_fact: `This meal contail high protein`
+    },
+    {
+        id: 2,
+        meal_name: 'Green Pea',
+        intro: `Don't forget to consume this in the morning, this meal can help you to fill your hungry, but with no worry.`,
+        nutrition_fact: `This pea will keep you keep sane`
+    },
+    {
+        id: 3,
+        meal_name: 'Oatmeal',
+        intro: `Fill your body with high quality grain`,
+        nutrition_fact: `Keep your body feels full all day long.`
+    },
+    
+
+]
+
+export default data_meal_details;
