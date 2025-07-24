12bf8c74928c22bd393a2e602901ac641c6ef2f7
sigma.lukman <auliya.lukman@sigma.co.id>
2025-07-24 09:25:59 +0700

    add page navigation

---
+++ src/components/page_navigation/PageNavigation.tsx
@@ -0,0 +1,23 @@
+
+import { Link } from "react-router-dom";
+import ButtonNav from "../buttons/button_nav/ButtonNav";
+import styles from "./PageNavigation.module.css";
+import { RoutesAll } from "../../App";
+
+const PageNavigation = () => {
+    // note: link route page mengikuti path di App.tsx
+    return (
+        <div>
+            <nav className={styles.navbar}>
+                <ul className={styles.menu}>
+                    <Link to={RoutesAll.meal_datatable}><ButtonNav children={<li>Home</li>} onclick={() => {}} /></Link>
+                    <Link to={RoutesAll.register_new_meal}><ButtonNav children={<li>Register New Meal</li>} onclick={() => {}} /></Link>
+                    
+                </ul>
+            </nav>
+
+        </div>
+    )
+};
+
+export default PageNavigation;
