a0900b69d0dea178a806e70b577676a9cae55424
sigma.lukman <auliya.lukman@sigma.co.id>
2025-11-07 22:05:49 +0700

    add tailwind css

--- src/components/meal_datatable/MealDataTable.tsx
+++ src/components/meal_datatable/MealDataTable.tsx
@@ -1,3 +1,4 @@
+
 import styles from './MealDataTable.module.css'
 
 type data_meal_datatable = {
@@ -8,13 +9,13 @@ type data_meal_datatable = {
 
 const MealDataTable = ({data_source }: { data_source: data_meal_datatable[] }) => {
     return (
-        <div className={styles.container}>
+        <div className="flex flex-col items-center">
             {/* create table here */}
-            <table className={styles.table}>
+            <table className="border-collapse w-1">
                 <thead>
                     <tr>
-                        <th className={styles.th}>Meal Name</th>
-                        <th className={styles.th}>Price</th>
+                        <th className="border border-solid border-white p-10 text-center">Meal Name</th>
+                        <th className="border border-solid border-white p-10 text-center">Price</th>
                     </tr>
                 </thead>
                 <tbody>
