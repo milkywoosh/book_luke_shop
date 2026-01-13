9d43883ac99d3bb8a163f0c7a86493e72c893d48
luke_aul <auliya.lukman@sigma.co.id>
2026-01-13 16:33:57 +0700

    fixing routing

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ import BaseLayout from './components/base_layout/BaseLayout.tsx';
 
 import About from './components/about/About.tsx';
 import OrderManagement from './components/order_management/OrderManagement.tsx';
-import AdminView from './components/admin_view/AdminView.tsx';
 import CustomerListView from './components/customer_list_view/CustomerListView.tsx';
 import CustomerDetailView from './components/customer_list_view/CustomerDetailList.tsx';
+import AdminDashboard from './components/admin_view/Dashboard.tsx';
 
 function App() {
 
@@ -20,7 +20,7 @@ function App() {
       <BaseLayout>
         <Routes>
           <Route path='/' element={<OrderManagement />} />
-          <Route path='/admin-dashboard' element={<AdminView />} />
+          <Route path='/admin-dashboard' element={<AdminDashboard />} />
           <Route path='/customer-list-view' element={<CustomerListView onSelect />} />
           <Route path='/customer-detail' element={<CustomerDetailView />} />
           <Route path='about' element={<About />} />
