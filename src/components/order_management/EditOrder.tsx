9de3c12826ccabea432c4fdf4dd47df8ae710759
luke_aul <auliya.lukman@sigma.co.id>
2026-01-17 00:18:10 +0700

    init edit order

---
+++ src/components/order_management/EditOrder.tsx
@@ -0,0 +1,20 @@
+// NOTE: only admin can edit
+
+const EditOrder = () => {
+
+    return (
+        <div>
+            {/*
+                NOTE: halaman ini utk edit 
+                1. munculkan dalam bentuk sepert form yang bisa diedit
+                2. bisa update / upload gambar
+                3. bisa edit harga
+                4. bisa edit final price
+                5. bisa edit pesanan selesai
+                
+             */}
+        </div>
+    );
+}
+
+export default EditOrder;
