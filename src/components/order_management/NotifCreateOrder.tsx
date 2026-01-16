dc3eea3e5268c57244d25f0c30351078cc3d8f1c
luke_aul <auliya.lukman@sigma.co.id>
2026-01-17 00:17:56 +0700

    create notif pop up after create order

---
+++ src/components/order_management/NotifCreateOrder.tsx
@@ -0,0 +1,25 @@
+
+const NotifCreateOrder = () => {
+
+    return (
+        <div>
+            {/*
+                NOTE: notif POP UPjika sukses atau gagal
+                MUNCULKAN hanya pada sebagian di tengah halaman, tanpa menghilangkan tampilan belakang
+                 
+                selelah selesai dan sukses, DIRECT ke dashboard
+
+                ===================
+                +                 +
+                +                 +
+                +                 +
+                +                 +
+                +                 +
+                ===================
+                
+             */}
+        </div>
+    );
+}
+
+export default NotifCreateOrder;
